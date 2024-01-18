01e09a54785106e7c981d75c636d2ce7d20b32ba
vladimir.grinenko <S1vjycnhbr>
2024-01-18 11:24:11 +0300

    feat: add checking for file availability in 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -10,12 +10,28 @@ const copyDir = () => {
       return console.error('Ошибка в создании папки');
     }
 
-    fs.readdir(initialDir, { withFileTypes: true }, (err, files) => {
+    fs.readdir(initialDir, { withFileTypes: true }, (err, initialFiles) => {
       if (err) {
         return console.error('Ошибка при чтении папки');
       }
 
-      files.forEach((file) => {
+      fs.readdir(endDir, { withFileTypes: true }, (errEnd, endFiles) => {
+        if (errEnd) {
+          return console.error('Ошибка при чтении финальной папки');
+        }
+
+        endFiles.forEach((endFile) => {
+          if (!initialFiles.some((file) => file.name === endFile.name)) {
+            fs.rm(path.join(endDir, endFile.name), (err) => {
+              if (err) {
+                return console.error('Ошибка при удалении файла');
+              }
+            });
+          }
+        });
+      });
+
+      initialFiles.forEach((file) => {
         const initialFliePath = path.join(initialDir, file.name);
         const endFliePath = path.join(endDir, file.name);
 
